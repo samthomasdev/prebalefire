dfae1d1acd68abe4fc3489158ce92a6e097fc814
Sam Thomas <samthomasdev@gmail.com>
2014-12-15 19:29:36 -0500

    fix bug where the tiles were just being drawn on top of each other

--- prototype/bunkerScene.py
+++ prototype/bunkerScene.py
@@ -15,7 +15,7 @@ spritesheets = dict(floor='ss_floor.png', character='hello.bmp')
 class BunkerScene:
 
 	def __init__(self):
-		self.bunker = bunker.Bunker()		
+		self.bunker = bunker.Bunker(width=30,numFloors=4)		
 		self.floorsrc = self.gen_floorsrc()
 		self.floordst = self.gen_floordst()
 
@@ -90,9 +90,17 @@ class BunkerScene:
 		self.ss_floor = factory.from_image(RESOURCES.get_path(spritesheets['floor']))
 
 	def draw(self, renderer):
-		for x in xrange(0,self.bunker.width):
-			for y in xrange(0,self.bunker.floorHeight):
-				renderer.copy(self.ss_floor, self.floorsrc[x][y], self.floordst[x][y])
+		h = FLOOR.TOP[3:][0] # tile height (in pixels)
+		floorOffsetY = h * self.bunker.floorHeight # height of each floor (in pixels)
+
+		for f in self.bunker.floors:
+			offset = f.floorNumber * floorOffsetY
+			for x in xrange(0,self.bunker.width):
+				for y in xrange(0,self.bunker.floorHeight):
+					src = self.floorsrc[x][y]
+					dst = self.floordst[x][y]
+					offsetRect = (dst[0], dst[1]+offset , dst[2], dst[3])
+					renderer.copy(self.ss_floor, src, offsetRect)
 
 
 
--- prototype/game.py
+++ prototype/game.py
@@ -8,52 +8,35 @@ from bunkerScene import BunkerScene
 RESOURCES = sdl2.ext.Resources(__file__, 'assets')
 
 
-rectWidth = 70
-rectHeight = 70
-
-x1 = 100
-y1 = 100
-
-
-
 def run():
-	# Window setup
+	## Window Setup ##
 	sdl2.ext.init()
 	window = sdl2.ext.Window('prebalefire prototype', size=(1200,900))
 	window.show()
-	# Create sprite factory (hardware accelerated)
+
+	## Render Initialization ## 
 	renderer = sdl2.ext.Renderer(window)
 	factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE,renderer=renderer)
-	# Create rendering system
 	spriterenderer = factory.create_sprite_render_system(window)
 
 
-	# Load 2d texture assets
+	## Load Resources ##
 	ss_floor = factory.from_image(RESOURCES.get_path('ss_floor.png'))
 
 	
-	
-	renderer.clear(color=sdl2.ext.Color(200,200,200))
-	spriterenderer.render(ss_floor)
-	# Spritesheet clip draw
-	srcrect = (140,0,rectWidth,rectHeight)
-	dstrect = (x1,y1,rectWidth,rectHeight)
-	renderer.copy(ss_floor, srcrect, dstrect)
-	#renderer.draw_rect(dstrect, color=sdl2.ext.Color(50,100,100))
-
-
-
+	## Draw ##	
+	renderer.clear(color=sdl2.ext.Color(140,150,150))
 
 	scene_bunker = BunkerScene()	
 	scene_bunker.loadSpritesheets(factory)
 	scene_bunker.draw(renderer)
 
-
-	# Display the changes
 	renderer.present()
 
 
-	# Main game loop
+
+
+	## Main Game Loop ##
 	running = True
 	while running:
 		events = sdl2.ext.get_events()
